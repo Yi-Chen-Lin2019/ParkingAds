164f02322b2559dc600354f7c9dd2bc41c12e9d9
Yi-Chen-Lin2019 <58112802+Yi-Chen-Lin2019@users.noreply.github.com>
2022-12-06 12:15:18 +0100

    add adService (mock), modify hotelService to make sure it replys to the right client's queue

--- adService.py
+++ adService.py
@@ -5,18 +5,21 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 
 channel = connection.channel()
+channel.exchange_declare(exchange='topic_find_parking', exchange_type='topic')
 
-def getAvailable(location):
-    response = '''
-    Greetings, welcome to {}
-    More hotels content comming...
-    '''.format(location)
+result = channel.queue_declare(queue='ad', exclusive=True)
+queue_name = result.method.queue
+
+binding_keys = "find.#"
+channel.queue_bind(exchange='topic_find_parking', queue=queue_name, routing_key=binding_keys)
+
+def getAvailable():
+    response = "This is an ad"
     return response
 
 def on_request(ch, method, props, body):
-    location = body
-    print('hotel service on request, location: ',location)
-    response = getAvailable(location)
+    print('ad service on request')
+    response = getAvailable()
 
     ch.basic_publish(exchange='topic_find_parking',
                      routing_key=props.reply_to,
@@ -26,7 +29,7 @@ def on_request(ch, method, props, body):
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='hotel', on_message_callback=on_request)
+channel.basic_consume(queue=queue_name, on_message_callback=on_request)
 
 print(" [x] Awaiting RPC requests")
 channel.start_consuming()
