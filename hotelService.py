1046f7af96a243e5f328babaf2fadc1bd580150c
Yi-Chen-Lin2019 <58112802+Yi-Chen-Lin2019@users.noreply.github.com>
2022-12-06 10:04:05 +0100

    add hotelService

--- hotelService.py
+++ hotelService.py
@@ -5,10 +5,15 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 
 channel = connection.channel()
+channel.exchange_declare(exchange='topic_find_parking', exchange_type='topic')
 
-channel.queue_declare(queue='rpc_queue')
+result = channel.queue_declare(queue='hotel', exclusive=True)
+queue_name = result.method.queue
 
-def getAvailableLots(location):
+binding_keys = "find.#"
+channel.queue_bind(exchange='topic_find_parking', queue=queue_name, routing_key=binding_keys)
+
+def getAvailable(location):
     response = '''
     Greetings, welcome to {}
     More hotels content comming...
@@ -17,19 +22,19 @@ def getAvailableLots(location):
 
 def on_request(ch, method, props, body):
     location = body
-    print(location)
+    print('hotel service on request, location: ',location)
     
-    response = getAvailableLots(location)
+    response = getAvailable(location)
 
-    ch.basic_publish(exchange='',
-                     routing_key=props.reply_to,
+    ch.basic_publish(exchange='topic_find_parking',
+                     routing_key='response.hotel',
                      properties=pika.BasicProperties(correlation_id = \
                                                          props.correlation_id),
                      body=str(response))
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='rpc_queue', on_message_callback=on_request)
+channel.basic_consume(queue=queue_name, on_message_callback=on_request)
 
 print(" [x] Awaiting RPC requests")
 channel.start_consuming()
