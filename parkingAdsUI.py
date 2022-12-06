c670ac1cebce06a01ae91653792d40742ea03424
Yi-Chen-Lin2019 <58112802+Yi-Chen-Lin2019@users.noreply.github.com>
2022-12-06 09:40:12 +0100

    now use topic exchange, parking service subscribe to find.# pattern to bind, and use corrid to match client

--- parkingAdsUI.py
+++ parkingAdsUI.py
@@ -18,14 +18,17 @@ class ParkingAdsClient():
             pika.ConnectionParameters(host='localhost'))
 
         self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange='topic_find_parking', exchange_type='topic')
 
-        result = self.channel.queue_declare(queue='', exclusive=True)
+        result = self.channel.queue_declare(queue='UI', exclusive=True)
         self.callback_queue = result.method.queue
+
+        binding_keys = "response"
+        self.channel.queue_bind(exchange='topic_find_parking', queue=self.callback_queue, routing_key=binding_keys)
 
         self.channel.basic_consume(
             queue=self.callback_queue,
-            on_message_callback=self.on_response,
-   )
+            on_message_callback=self.on_response)
 
         self.response = None
         self.corr_id = None
@@ -33,13 +36,15 @@ class ParkingAdsClient():
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
             self.response = body
-
+        else:
+            print('wrong corrid')
     def call(self):
         self.response = None
         self.corr_id = str(uuid.uuid4())
+
         self.channel.basic_publish(
-            exchange='',
-            routing_key='rpc_queue',
+            exchange='topic_find_parking',
+            routing_key='find.parking',
             properties=pika.BasicProperties(
                 reply_to=self.callback_queue,
                 correlation_id=self.corr_id,
--- parkingService.py
+++ parkingService.py
@@ -5,31 +5,35 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 
 channel = connection.channel()
+channel.exchange_declare(exchange='topic_find_parking', exchange_type='topic')
 
-channel.queue_declare(queue='rpc_queue')
+result = channel.queue_declare(queue='parking', exclusive=True)
+queue_name = result.method.queue
+
+binding_keys = "find.#"
+channel.queue_bind(exchange='topic_find_parking', queue=queue_name, routing_key=binding_keys)
 
 def getAvailableLots(location):
     response = '''
     Hi, welcome to {}
     More content comming...
-    '''.format(location)
+    ''' .format(location)
     return response
 
 def on_request(ch, method, props, body):
     location = body
-
-    print(location)
+    print('parking service on request, location: ',location)
     response = getAvailableLots(location)
 
-    ch.basic_publish(exchange='',
-                     routing_key=props.reply_to,
+    ch.basic_publish(exchange='topic_find_parking',
+                     routing_key='response',
                      properties=pika.BasicProperties(correlation_id = \
                                                          props.correlation_id),
                      body=str(response))
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='rpc_queue', on_message_callback=on_request)
+channel.basic_consume(queue=queue_name, on_message_callback=on_request)
 
 print(" [x] Awaiting RPC requests")
 channel.start_consuming()
